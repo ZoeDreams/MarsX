92045411f59cc5baefbffb67357211d4816f108c
Kara Rawson <rawsonkara@gmail.com>
2021-02-14 22:14:26 -0500

    added prettier configuration for lume and fixed webpack constants

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from 'electron'
-declare const MAIN_WINDOW_WEBPACK_ENTRY: any
-declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: any
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -14,6 +14,7 @@ const createWindow = (): void => {
     width: 800,
     show: false,
     webPreferences: {
+      nodeIntegration: true,
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     }
   })
@@ -24,6 +25,7 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 
+  // opens the window on the users desktop
   mainWindow.show()
 
   // Open the DevTools.
